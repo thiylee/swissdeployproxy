56eb7927c7d688340e35749111147b55c095e846
thiylee thi <thiylethi48@gmail.com>
2024-08-15 16:08:31 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x42b856688da4d3abDcFc82e0f2671A29f26854ab'
+export const ProxyAdmin = '0xBc6F79D95aDE3efAba07CE1Ec17D763186054B88'
+export const SWTRImplementation = '0x6Efd8eED2780D62a936345Cb1B6ce8362ed7cD38'
     
